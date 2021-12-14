afed8935deb7c2ac8ed71c1d879e0fd9d7787715
janhavi-dave <91345503+janhavi-dave@users.noreply.github.com>
2021-12-14 17:45:49 +0530

    Updated Server.js

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-console.log("welcome to the property Management Api system");
+var express = require('express');
+var app = express();
+const mysql = require('mysql');
+router = express.Router();
+propertyRoute = require('./routes/property.route');
+app.use('/api', propertyRoute);
+
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'property'
+  });
+  connection.connect((err) => {
+    if (err){
+        res.status(503).send("there were something went wrong");
+    }
+    console.log('Connected to MySQL Server!');
+  });
+
+const port = process.env.PORT || 8000;
+
+const server = app.listen(port, function(){
+  console.log('Listening on port ' + port);
+});
+
