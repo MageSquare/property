195637b3387ec5a6bec5ebd4e0331abd708c2896
MageSquare <92527816+MageSquare@users.noreply.github.com>
2021-12-22 22:33:11 +0530

    server.js update for cors policy error

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express'),
 bodyParser = require('body-parser'),
 propertyRoutes= require('./src/routes/property.routes'),
+cors = require('cors'),
 userRoutes=require('./src/routes/user.routes');
 
 app =express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(cors());
+
 
 // Routes For API
   app.use('/api',propertyRoutes);
@@ -13,7 +16,7 @@ app.use(bodyParser.json());
 // Routes For API
 
 // Listing on Port
-  const port = process.env.PORT || 8000;
+  const port = process.env.PORT || 8520;
   const server = app.listen(port, function(){
     var host = server.address().address;
     var port = server.address().port;
