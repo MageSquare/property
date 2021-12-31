3f9657202b2634d51c02eed41d47b64afe4dccd2
janhavi-dave <91345503+janhavi-dave@users.noreply.github.com>
2021-12-31 15:07:52 +0530

    Create user.routes.js

---
+++ src/routes/user.routes.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const Providers = require('../models/providers.model');
+const userRoutes = express.Router();
+
+// get Property created by user
+    userRoutes.route('/get_my_created_property').get(function(req,res){
+        let currentUser=2;
+        let per_page=req.query.per_page;
+        let curr_page=req.query.curr_page;
+
+            Profiles.userProperties(currentUser,per_page,curr_page,function(err, data) {    
+            if (err){
+                res.status(400).send("No Data found");
+            }
+            else
+            {
+                res.status(200).send(data);
+            }
+        });
+    });
+// get Property created by user
+
+// get current user login detail 
+    userRoutes.route('/get').get(function(req,res){
+        var id = req.body.userId;
+        Providers.getUser(id,function(err,data){
+            if (err){
+                res.status(400).send(err);
+            }
+            else
+            {
+                res.status(200).send(data);
+            }
+        });
+    });
+// get current user login detail 
+module.exports = userRoutes;
