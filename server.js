f56234754bf703ec3bc5381d8d3bea27c830e46f
janhavi-dave <91345503+janhavi-dave@users.noreply.github.com>
2022-01-01 15:39:36 +0530

    timestamp format

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express'),
+bodyParser = require('body-parser'),
+cors = require('cors'),
+jwt = require('jsonwebtoken'),
+propertyRoutes= require('./src/routes/property.routes'),
+morgan = require('morgan'),
+userRoutes=require('./src/routes/user.routes');
+
+require('dotenv').config();
+app =express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan('tiny'));
+
+// Routes For API
+  app.use('/api',propertyRoutes);
+  app.use('/api/user',validateRequest,userRoutes);
+// Routes For API
+
+// Listing on Port
+  const port = process.env.PORT || 8520;
+  const server = app.listen(port, function(){
+    var port = server.address().port;
+    console.log("App listening at port:%s", port)
+  });
+// Listing on Port
+
+
+function validateRequest(req, res, next) {
+    const bearerHeader = req.headers['authorization'];
+    //check if bearer is undefined
+    if(typeof bearerHeader !== 'undefined'){
+        //split the space at the bearer
+        const bearer = bearerHeader.split(' ');
+        //Get token from string
+        const bearerToken = bearer[1];
+        jwt.verify(bearerToken, process.env.JWT_SECRET, function(err, decoded) {
+          if (err) {
+            res.status(403).json({msg: "Unauthorized"});
+          }else{
+            // get value of user whicha are set when create token
+              req.body.userId = decoded.userId;
+            // get value of user whicha are set when create token
+            next();
+          }
+        });
+    }else{
+       res.status(403).json({msg: "Unauthorized"});
+    }
+  }
