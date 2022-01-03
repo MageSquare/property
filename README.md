39e9ecd3bc1ec494e84b9d03f1e57509b858e078
MageSquare <92527816+MageSquare@users.noreply.github.com>
2022-01-03 11:26:42 +0530

    property.routes.js, providers.model.js and package.json files are updated.
    Have updated property.routes.js, providers.model.js and package.json with forget and reset password within one API. Made function reusable of forgetPassword.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Property Management
+
+Property Management 
+
+
+Download 
+	Run command npm install && nodemon server
+
+Add "openimmo" folder in root 
+	create sub folder as per provider id 
+		Add zip into the your provider id folders
+
+
+Create One Folder "unzip" into the root 
+
--- src/routes/property.routes.js
+++ src/routes/property.routes.js
@@ -61,7 +61,6 @@ const { body,check, validationResult } = require('express-validator');
   });
 //Fetch Property By Id by Get Method
 
-
 // Get list of all properties by folder by POST method
   propertyRoutes.route('/properties').post(function(req,res){
       let provider_id = req.body.value;
@@ -80,7 +79,6 @@ const { body,check, validationResult } = require('express-validator');
 
   });
 // Get list of all properties by folder by POST method
-
 
 // Delete created property
   propertyRoutes.route('/delete_my_created_property').delete(function(req,res){
@@ -117,97 +115,6 @@ const { body,check, validationResult } = require('express-validator');
   });
 // Get list of all verkauft properties
 
-// Register
-propertyRoutes.route('/register').post(async function(req,res){
-
-    await check('email',"Email is required").notEmpty().run(req);
-    await check('password',"Password is required").notEmpty().run(req);
-    await check('email',"Enter valid email Address").isEmail().run(req);
-    await check('firstname',"Firstname is required").notEmpty().run(req);
-    await check('lastname',"Lastname is required").notEmpty().run(req);
-    await check('password').isLength({ min: 8 }).withMessage('Password must be at least 8 chars long').run(req);
-    await check('password').matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d@$.!%*#?&]/).withMessage('Please enter a password at least 8 character and contain At least one uppercase.At least one lower case.At least one special character.').run(req);
-
-   const errors = validationResult(req);
-
-   let firstname = req.body.firstname,
-    lastname = req.body.lastname,
-    email = req.body.email,
-    password = req.body.password,
-    phone = req.body.phone,
-    firma = req.body.firma,
-    provider_dir = req.body.provider_dir,
-    state = req.body.state,
-    city = req.body.city,
-    pic = req.body.pic;
-
-    let providers = new Providers(req.body);
-
-    if(phone == null || phone == ''){
-
-        providers.phone = null;
-    }
-    else{
-        providers.phone = phone;
-    }
-
-    if(firma == null || firma == ''){
-      
-        providers.firma = null;
-    }
-    else{
-        providers.firma = firma;
-    }
-
-    if(state == null || state == ''){
-      
-        providers.state = null;
-    }
-    else{
-        providers.state = state;
-    }
-
-    if(city == null || city == ''){
-      
-        providers.city = null;
-    }
-    else{
-        providers.city = city;
-    }
-
-    if(state == null || state == ''){
-      
-        providers.pic = null;
-    }
-    else{
-        providers.pic = pic;
-    }
-
-    providers.fullname = firstname.concat(' ', lastname);
-    providers.provider_dir = null;
-    providers.role = 0;
-
-    if (!errors.isEmpty()) {
-            res.status(400).send(errors);
-        }
-
-    else
-    {
-        Providers.register(providers,function(err, data) {  
-          if (err){
-              res.status(400).send(err);
-          }
-          else
-          {
-             res.status(200).json(data);
-          }
-
-        });
-    }
-    
-
-});
-// register
 
 // Login
   propertyRoutes.route('/login'
@@ -236,14 +143,122 @@ propertyRoutes.route('/register').post(async function(req,res){
   });
 // Login
 
+
+// Import All Properties Start                   
+
+  propertyRoutes.route('/import-properties').get(function(req,res){
+      Immobilie.importProperty(function(err,immobilie){
+            if (err){
+                res.status(400).send(err);
+            }
+            else
+            {
+                res.status(200).send(immobilie);
+            }
+      });
+  });
+
+// Import All Properties End
+
+// REgister 
+  propertyRoutes.route('/register').post(async function(req,res){
+      await check('email',"Email is required").notEmpty().run(req);
+      await check('password',"Password is required").notEmpty().run(req);
+      await check('email',"Enter valid email Address").isEmail().run(req);
+      await check('firstname',"Firstname is required").notEmpty().run(req);
+      await check('lastname',"Lastname is required").notEmpty().run(req);
+      // await check('password').isLength({ min: 8 }).withMessage('Password must be at least 8 chars long').run(req);
+      await check('password').matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d@$.!%*#?&]/).isLength({ min: 8 }).withMessage('Please enter a password at least 8 character and contain At least one uppercase.At least one lower case.At least one special character.').run(req);
+
+      const errors = validationResult(req);
+
+      let firstname = req.body.firstname,
+      lastname = req.body.lastname,
+      email = req.body.email,
+      password = req.body.password,
+      phone = req.body.phone,
+      firma = req.body.firma,
+      provider_dir = req.body.provider_dir,
+      state = req.body.state,
+      city = req.body.city,
+      pic = req.body.pic;
+
+      let providers = new Providers(req.body);
+
+      if(phone == null || phone == ''){
+
+          providers.phone = null;
+      }
+      else{
+          providers.phone = phone;
+      }
+
+      if(firma == null || firma == ''){
+        
+          providers.firma = null;
+      }
+      else{
+          providers.firma = firma;
+      }
+
+      if(state == null || state == ''){
+        
+          providers.state = null;
+      }
+      else{
+          providers.state = state;
+      }
+
+      if(city == null || city == ''){
+        
+          providers.city = null;
+      }
+      else{
+          providers.city = city;
+      }
+
+      if(state == null || state == ''){
+        
+          providers.pic = null;
+      }
+      else{
+          providers.pic = pic;
+      }
+
+      providers.fullname = firstname.concat(' ', lastname);
+      providers.provider_dir = null;
+      providers.role = 0;
+
+      if (!errors.isEmpty()) {
+              res.status(400).send(errors);
+          }
+
+      else
+      {
+          Providers.register(providers,function(err, data) {  
+            if (err){
+                res.status(400).send(err);
+            }
+            else
+            {
+               res.status(200).json(data);
+            }
+
+          });
+      }
+
+  });
+// register
+
 // Forgot Password
  propertyRoutes.route('/forget_password').post(function(req,res){
    
    let origin_server = req.headers.host;
    let url = req.baseUrl;
    let receiver_email = req.body.email;
+   let forgetandreset = false;
 
-    Providers.forgetPassword(origin_server,url,receiver_email,function(err, data) {    
+    Providers.forgetPassword(origin_server,url,receiver_email,forgetandreset,function(err, data) {    
 
         if (err){
             res.status(400).send(err);
@@ -260,15 +275,12 @@ propertyRoutes.route('/register').post(async function(req,res){
 // Reset Password
  propertyRoutes.route('/reset_password').post(async function(req,res){
 
-             await check('password').isLength({ min: 8 }).withMessage('Password must be at least 8 chars long').run(req);
-             await check('password').matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d@$.!%*#?&]/).withMessage('Please enter a password at least 8 character and contain At least one uppercase.At least one lower case.At least one special character.').run(req);
-            
-              const errors = validationResult(req);
+             await check('password').matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d@$.!%*#?&]/).isLength({ min: 8 }).withMessage('Please enter a password at least 8 character and contain At least one uppercase.At least one lower case.At least one special character.').run(req);
+             const errors = validationResult(req);
                
               if (!errors.isEmpty()) {
                     res.status(400).send(errors);
               }
-
               else{
 
                 let token = req.query.token;
@@ -279,7 +291,6 @@ propertyRoutes.route('/register').post(async function(req,res){
                          res.status(400).send('Sorry! Your Token May Expired.');
                      }
                      else{
-
                          let user_id = req.body.id;
                          let new_pswd = req.body.password;
                          Providers.resetPassword(user_id,new_pswd, function(err, data) {   
@@ -300,5 +311,27 @@ propertyRoutes.route('/register').post(async function(req,res){
 
 });
 // Reset Password
+
+// Forgot Password
+ propertyRoutes.route('/forget_reset_password').post(function(req,res){
+   
+   let origin_server = req.headers.host;
+   let url = req.baseUrl;
+   let receiver_email = req.body.email;
+   let forgetandreset = true;
+
+    Providers.forgetPassword(origin_server,url,receiver_email,forgetandreset,function(err, data) {    
+
+        if (err){
+            res.status(400).send(err);
+        }
+        else
+        {
+          res.status(200).json({message : data});
+        }
+    });
+
+});
+// Forgot Password
 
 module.exports = propertyRoutes;
